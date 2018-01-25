25a4d47dc6591441b4daa2d80ac8a51562bf93b9
ditero <anelesigenu@gmail.com>
2018-01-25 13:42:44 +0200

    Accounts Component Functionn

--- src/js/jet-composites/panel-component/viewModel.js
+++ src/js/jet-composites/panel-component/viewModel.js
@@ -8,19 +8,61 @@ define(
 
     function ExampleComponentModel(context) {
         var self = this;
+        self.firsttUser = ko.observable();
+        self.userAccounts = ko.observableArray([]);
+        self.envDescription = ko.observableArray([]);
+        self.secondUser = ko.observable();
+        self.thirdUser = ko.observable();
         self.composite = context.element;
         //Example observable
         // self.messageText = ko.observable('Hello from Example Component');
 
-        context.props.then(function (propertyMap) {
-            //Store a reference to the properties for any later use
-            self.properties = propertyMap;
+    // REquest data from the back-end server
+    $.ajax({
+      url: "http://localhost:5000/jde/get_config", // <<- JD Edwards API token service
+      type: 'get', // <<- the method that we using
+      data: JSON,
+      contentType: 'application/json', // <<- telling server how we are going to communicate
+      fail: function(xhr, textStatus, errorThrown) {
 
-            //Parse your component properties here
+        console.log(errorThrown, textStatus, xhr); //  <<- log any http errors to the console
 
-        });
-    };
+      }
+    }).done(function(data, textStatus, xhr) {
 
+      for (var key in data) {
+          // self.accounts.push(data.description)
+          // console.log(data);
+          var myAccounts = data[0].accounts;
+          // console.log(myAccounts);
+          for (var i = 0; i < myAccounts.length; i++) {
+            // console.log(myAccounts[i].environments[i].ais)
+            self.userAccounts.push(myAccounts[i])
+            self.envDescription.push(myAccounts[i].environments[i])
+          }
+      }
+
+      // self.firsttUser(data[0].accounts[0].description);
+      //
+      // self.secondUser(data[0].accounts[1].description);
+      //
+      // self.thirdUser(data[0].accounts[2].description)
+
+
+      // console.log(data[0].accounts);
+
+      console.log("*****************");
+
+      var uiData = JSON.stringify(data)
+      // console.log(uiData);
+
+      // for (var i = 0; i < uiData.length; i++) {
+      //   // console.log(uiData);
+      // }
+    });
+
+    // self.header = ko.observable(sehdBind);
+    // self.header  = hdBind;
     //Lifecycle methods - uncomment and implement if necessary
     //ExampleComponentModel.prototype.activated = function(context){
     //};
@@ -33,6 +75,14 @@ define(
 
     //ExampleComponentModel.prototype.detached = function(context){
     //};
+    context.props.then(function (propertyMap) {
+      //Store a reference to the properties for any later use
+      self.properties = propertyMap;
+
+      //Parse your component properties here
+
+    });
+  };
 
     return ExampleComponentModel;
 });
